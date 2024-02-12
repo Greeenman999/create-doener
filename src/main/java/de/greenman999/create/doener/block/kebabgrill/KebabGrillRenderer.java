48b6372a85aa18de296e4d39b21d3125c1daa03c
Greenman999 <mail@greenman999.de>
2024-02-12 23:45:30 +0100

    feat: uhhh

--- src/main/java/de/greenman999/create/doener/block/kebabgrill/KebabGrillRenderer.java
+++ src/main/java/de/greenman999/create/doener/block/kebabgrill/KebabGrillRenderer.java
@@ -1,11 +1,19 @@
 package de.greenman999.create.doener.block.kebabgrill;
 
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import com.simibubi.create.foundation.render.CachedBufferer;
+import com.simibubi.create.foundation.render.SuperByteBuffer;
+import de.greenman999.create.doener.block.ModPartialModels;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class KebabGrillRenderer extends KineticBlockEntityRenderer<KebabGrillBlockEntity> {
     public KebabGrillRenderer(BlockEntityRendererProvider.Context context) {
         super(context);
     }
 
+    @Override
+    protected SuperByteBuffer getRotatedModel(KebabGrillBlockEntity  be, BlockState state) {
+        return CachedBufferer.partial(ModPartialModels.KEBAB_SKEWER, state);
+    }
 }
