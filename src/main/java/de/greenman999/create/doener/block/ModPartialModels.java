3cae4283074cda1c074262337f38a05e0e05d9ad
Greenman999 <mail@greenman999.de>
2024-02-12 23:52:21 +0100

    fix: something1

--- src/main/java/de/greenman999/create/doener/block/ModPartialModels.java
+++ src/main/java/de/greenman999/create/doener/block/ModPartialModels.java
@@ -6,7 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class ModPartialModels {
 
-    public static final PartialModel KEBAB_SKEWER = block("kebab_skewer");
+    public static final PartialModel KEBAB_SKEWER = block("kebab_skewer_10");
 
     private static PartialModel block(String path) {
         return new PartialModel(new ResourceLocation(CreateDoener.MOD_ID, "block/" + path));
